0ec52923f5c43e1e3b1cb5a9e38793966d9e6476
russianxiii <russianxiii@ozon.ru>
2021-11-28 16:10:28 +0300

    [code-first]: get pets by status with generated api

---
+++ src/api/api.ts
@@ -0,0 +1,3 @@
+import {Api} from "@/api/petsApi";
+
+export const petService = new Api().pet
--- src/components/Pets.vue
+++ src/components/Pets.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
+    <div
+        v-for="pet in pets"
+        :key="pet.id"
+    >
+      {{pet.name}} - {{pet.status}}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import {petService} from "@/api/api";
+import {Pet} from "@/api/petsApi";
 
 export default Vue.extend({
   name: 'Pets',
-  created() {
-
+  data() {
+    return {
+      pets: [] as Array<Pet>
+    }
+  },
+  async created() {
+    const response = await petService.findPetsByStatus({
+      status: ['pending']
+    })
+    this.pets = response.data
   }
 });
 </script>
